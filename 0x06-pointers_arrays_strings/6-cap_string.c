d4738ced00849a7e1facd0ac898f87d2cc6e7b19
Moses FIAWOO <mosesfiawoo@gmail.com>
2022-04-24 20:34:19 -0700

    Strncpy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string.
  * @s: input string.
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
@@ -27,5 +26,5 @@ char *cap_string(char *)
 		}
 		count++;
 	}
-	return (dest);
+	return (s);
 }
